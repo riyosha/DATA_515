d04dd9250bf2aa3df5c0cd2f01b842bac381ddb7
Edgar Lopez <eloxacto@gmail.com>
2025-03-10 10:41:58 -0700

    Add simple video page

---
+++ frontend/src/MovieReview/Video.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const Video = ({ videoPath }) => {
+  return (
+    <div className="video-container">
+      <p>Hello! Video path: {videoPath}</p>
+    </div>
+  );
+};
+
+Video.propTypes = {
+  videoPath: PropTypes.string.isRequired
+};
+
+export default Video;
