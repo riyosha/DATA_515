6e7dcfec1035fa2f9e23b6b19979f019422618fc
dyutivartak <vartakdyuti@gmail.com>
2025-03-13 15:11:52 -0700

    Updated reviews code

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,9 +1,22 @@
 """
 API for scraping movie details and reviews from Letterboxd
 """
-
+import os
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-from src.helpers.scrapers import movie_details_scraper,scrape_reviews
+from helpers.scrapers import movie_details_scraper,scrape_reviews
+from helpers.letterboxd_analyzers import LetterboxdReviewAnalyzer
+
+
+load_dotenv()
+# Set up Google Gemini API key
+'''    api_keys_rio = os.getenv("GEMINI_API_KEY_RIO", "").split(",")
+    api_keys_sai = os.getenv("GEMINI_API_KEY_SAI", "").split(",")
+    for i in range(3): '''
+api_key1 = os.getenv("GEMINI_API_KEY_RIO2")
+api_key2 = os.getenv("GEMINI_API_KEY_SAI2")
+
+analyze = LetterboxdReviewAnalyzer()
 
 app = Flask(__name__)
 
@@ -40,9 +53,13 @@ def reviews():
         if not film_url:
             return jsonify({'error': 'film_url is required'}), 400
         reviews = scrape_reviews(film_url)
+        reviews_text = analyze.read_reviews(reviews)
+        summary, aspects = analyze.get_results(reviews_text,api_key1,api_key2)
+
 
         return jsonify({
-            'reviews': reviews,
+            'summary': summary,
+            'aspects': aspects
         })
 
     except Exception as e:
