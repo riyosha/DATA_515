4d9706b2de77cd819da4a93a2a1330d64b11ccc5
Edgar Lopez <eloxacto@gmail.com>
2025-03-13 03:14:23 -0700

    Add Home landing page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App.jsx';
+import Landing from './Landing.jsx';
+import MovieInfo from './MovieReview/MovieInfo.jsx';
+import Roast from './Roast/Roast.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/movie-info" element={<MovieInfo />} />
+        <Route path="/roast" element={<Roast />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
